68c1ad9d2bdc69acf1ad7e2b076d6c3f730fcd33
lilmeddy <aghedopriscilla@gmail.com>
2023-10-02 06:27:22 +0100

    sign up and sign in done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,210 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useFormik } from "formik";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import * as yup from "yup";
 
-function App() {
+const App = () => {
+  const navigate = useNavigate();
+  const url = "http://localhost:12000/users/signup";
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      c_password: "",
+      firstName: "",
+      lastName: "",
+      phone: "",
+    },
+    validationSchema: yup.object({
+      email: yup
+        .string()
+        .required("Email is required")
+        .email("Must be a valid email"),
+      password: yup
+        .string()
+        .required("Password field cannot be empty")
+        .min(6, "Password must be at least 6 characters")
+        .max(8, "Password must not be more than 8 characters"),
+      c_password: yup
+        .string()
+        .required("Confirm password field cannot be empty")
+        .oneOf([yup.ref("password"), null], "Your password does not match"),
+      firstName: yup
+        .string("First name must be a string")
+        .min(2, "First name is too short")
+        .max(20, "First name is too long")
+        .required("Firstname is required")
+        .matches(/^[a-zA-Z]+$/, "First name must only contain letters"),
+      lastName: yup
+        .string("Last name must be a string")
+        .min(2, "Last name is too short")
+        .max(20, "Last name is too long")
+        .required("Lastname is required")
+        .matches(/^[a-zA-Z]+$/, "Last name must only contain letters"),
+    }),
+    onSubmit(values) {
+      console.log(values);
+      axios
+        .post(url, values)
+        .then((res) => {
+          console.log(res);
+          alert("Registration Successfull");
+          navigate("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <>
+        <form
+          onSubmit={formik.handleSubmit}
+          className="w-3/6 border-2 border-blue-400 mx-auto rounded-md p-5"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <h2 className="text-blue-400 text-2xl font-semibold">Register</h2>
+          <div className="relative z-0 w-full mb-6 group">
+            <input
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              type="email"
+              name="email"
+              id="floating_email"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+            />
+            <label
+              for="floating_email"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Email address
+            </label>
+            {formik.errors.email ? (
+              <small className="text-red-500 font-semibold">
+                {formik.errors.email}
+              </small>
+            ) : null}
+          </div>
+          <div className="relative z-0 w-full mb-6 group">
+            <input
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              type="password"
+              name="password"
+              id="floating_password"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_password"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Password
+            </label>
+            {formik.errors.password ? (
+              <small className="text-red-500 font-semibold">
+                {formik.errors.password}
+              </small>
+            ) : null}
+          </div>
+          <div className="relative z-0 w-full mb-6 group">
+            <input
+              onChange={formik.handleChange}
+              value={formik.values.c_password}
+              type="password"
+              name="c_password"
+              id="floating_repeat_password"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_repeat_password"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Confirm password
+            </label>
+            {formik.errors.c_password ? (
+              <small className="text-red-500 font-semibold">
+                {formik.errors.c_password}
+              </small>
+            ) : null}
+          </div>
+          <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                onChange={formik.handleChange}
+                value={formik.values.firstName}
+                type="text"
+                name="firstName"
+                id="firstName"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required
+              />
+              <label
+                for="floating_firstName"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                First name
+              </label>
+            </div>
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                onChange={formik.handleChange}
+                value={formik.values.lastName}
+                type="text"
+                name="lastName"
+                id="floating_lastName"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required
+              />
+              <label
+                for="floating_lastName"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Last name
+              </label>
+            </div>
+          </div>
+          <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                onChange={formik.handleChange}
+                value={formik.values.phone}
+                type="tel"
+                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                name="phone"
+                id="floating_phone"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                required
+              />
+              <label
+                for="floating_phone"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Phone number (123-456-7890)
+              </label>
+            </div>
+        
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+        </form>
+      </>
+    </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Login from './Login';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route,Routes} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path='/login' element={<Login/>}/>
+
+    </Routes>
+    </BrowserRouter>
+   
   </React.StrictMode>
 );
 
