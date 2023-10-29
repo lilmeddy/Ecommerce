f43306cb4c29d803253988911f02b5100bb274a6
lilmeddy <aghedopriscilla@gmail.com>
2023-10-29 05:58:53 +0100

    loader done styling time

--- src/Load.js
+++ src/Load.js
@@ -1,14 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Load.css";
 
 const Load = () => {
-    const [loading, setLoading] = useState(true);
-    const navigate = useNavigate(); 
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+      navigate("/app");
+    }, 3000);
+  }, [navigate]);
+
   return (
     <>
-    
+      <div className="loader">
+        {loading ? (
+            <main>
+                <div className="img">
+                    <img src={require("./Images/Ace2.jpeg")} alt="" />
+                </div>
+            <h1>ACE</h1>
+          <div className="bar">
+            <span className="load"></span>
+          </div>
+            </main>
+           
+        ) : null}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Load
+export default Load;
